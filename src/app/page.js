5e8a211efb268cc70db510d23f5d314bf566a0de
naveenbishwas <naveenbishwas4@gmail.com>
2025-08-05 15:15:31 +0530

    done-layout

--- src/app/page.js
+++ src/app/page.js
@@ -3,8 +3,9 @@
 import Image from "next/image";
 import "./page.css";
 import Header from "@/components/Header/page";
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 import Link from "next/link";
+import Footer from "@/components/footer/page";
 
 const features = [
   {
@@ -65,8 +66,109 @@ const steps = [
   },
 ];
 
+const integrations = [
+  {
+    href: "/integration/syncpro",
+    title: "Sync pro",
+    description: "Sync products and customers.",
+    icon: "/init-1.svg",
+  },
+  {
+    href: "/integration/syncmaster",
+    title: "Sync master",
+    description: "Automate sales emails.",
+    icon: "/init-2.svg",
+  },
+  {
+    href: "/integration/syncedge",
+    title: "Sync edge",
+    description: "Integrate with your CRM.",
+    icon: "/init-3.svg",
+  },
+  {
+    href: "/integration/linksync",
+    title: "Link sync",
+    description: "Automate tasks easily.",
+    icon: "/init-4.svg",
+  },
+  {
+    href: "/integration/linkhub",
+    title: "Link hub",
+    description: "Grow your email list.",
+    icon: "/init-1.svg",
+  },
+  {
+    href: "/integration/fusionsync",
+    title: "Fusion sync",
+    description: "Sync products and customers.",
+    icon: "/init-2.svg",
+  },
+  {
+    href: "/integration/fusionlink",
+    title: "Fusion link",
+    description: "Boost eCommerce sales.",
+    icon: "/init-3.svg",
+  },
+  {
+    href: "/integration/datasyncpro",
+    title: "Data sync pro",
+    description: "Track campaign data.",
+    icon: "/init-4.svg",
+  },
+  {
+    href: "/integration/datalinker",
+    title: "Data linker",
+    description: "Use email lists to create targeted ad campaigns.",
+    icon: "/init-1.svg",
+  },
+  {
+    href: "/integration/datahub",
+    title: "Data hub",
+    description: "The more tailored your messaging can be",
+    icon: "/init-2.svg",
+  },
+];
+
+const testimonials = [
+  {
+    image: "/testimonial-1.avif",
+    name: "Sarah Mitchell",
+    role: "UI/UX Designer",
+    companyLogo: "/logo-optimal.svg",
+    feedback:
+      "Their data-driven approach helped us improve profitability and reduce unnecessary expenditures by 20%. They're the real deal!",
+  },
+  {
+    image: "/testimonial-2.avif",
+    name: "Alex Johnson",
+    role: "Sales Director",
+    companyLogo: "/logo-enterprise.svg",
+    feedback:
+      "We've streamlined our processes, cut costs, and significantly improved customer satisfaction. It’s been a game-changer for our business!",
+  },
+
+  {
+    image: "/testimonial-3.avif",
+    name: "Alex Johnson",
+    role: "Sales Director",
+    companyLogo: "/logo-enterprise.svg",
+    feedback:
+      "We've streamlined our processes, cut costs, and significantly improved customer satisfaction. It’s been a game-changer for our business!",
+  },
+  {
+    image: "/testimonial-4.avif",
+    name: "Alex Johnson",
+    role: "Sales Director",
+    companyLogo: "/logo-enterprise.svg",
+    feedback:
+      "We've streamlined our processes, cut costs, and significantly improved customer satisfaction. It’s been a game-changer for our business!",
+  },
+];
+
 export default function Home() {
   const containerRef = useRef(null);
+  const [startIndex, setStartIndex] = useState(0);
+  const [cardsPerView, setCardsPerView] = useState(2);
 
   const scrollLeft = () => {
     if (containerRef.current) {
@@ -83,6 +185,37 @@ export default function Home() {
         left: containerRef.current.offsetWidth,
         behavior: "smooth",
       });
+    }
+  };
+
+  useEffect(() => {
+    const updateView = () => {
+      if (window.innerWidth <= 768) {
+        setCardsPerView(1);
+      } else {
+        setCardsPerView(2);
+      }
+    };
+
+    updateView(); // Initial check
+    window.addEventListener("resize", updateView);
+    return () => window.removeEventListener("resize", updateView);
+  }, []);
+
+  const visibleTestimonials = testimonials.slice(
+    startIndex,
+    startIndex + cardsPerView
+  );
+
+  const handleNext = () => {
+    if (startIndex + cardsPerView < testimonials.length) {
+      setStartIndex(startIndex + cardsPerView);
+    }
+  };
+
+  const handlePrev = () => {
+    if (startIndex - cardsPerView >= 0) {
+      setStartIndex(startIndex - cardsPerView);
     }
   };
 
@@ -309,6 +442,222 @@ export default function Home() {
           </div>
         ))}
       </section>
+
+      <section className="integration">
+        <div className="integration-df">
+          <div className="integration-left">
+            {["up", "down", "up"].map((direction, index) => (
+              <div
+                key={index}
+                className={`integration-list-marquee ${direction}`}
+              >
+                <div className="integration-marquee-inner">
+                  {integrations.map((item, idx) => (
+                    <div key={idx} className="integration-item">
+                      <Link
+                        href={item.href}
+                        className="integration-link"
+                        aria-label={item.title}
+                      >
+                        <div className="integration-icon">
+                          <Image
+                            src={item.icon}
+                            alt={item.title}
+                            width={40}
+                            height={40}
+                            loading="eager"
+                          />
+                        </div>
+                        <div>
+                          <h2 className="integration-title">{item.title}</h2>
+                          <p className="integration-description">
+                            {item.description}
+                          </p>
+                        </div>
+                      </Link>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            ))}
+          </div>
+
+          <div className="integration-right">
+            <h1>Effortless integration, maximum impact</h1>
+            <p>
+              Whether it’s eCommerce, CRM, or social media, we’ve made it easy
+              to connect your tools and streamline your marketing efforts.
+            </p>
+            <Link href="#">
+              <button className="cta-button" id="integration-btn">
+                Explore all integrations
+                <span className="arrow-wrapper">
+                  <span className="arrow first-arrow">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth="1.5"
+                      stroke="currentColor"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3"
+                      />
+                    </svg>
+                  </span>
+                  <span className="arrow second-arrow">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth="1.5"
+                      stroke="currentColor"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3"
+                      />
+                    </svg>
+                  </span>
+                </span>
+              </button>
+            </Link>
+          </div>
+        </div>
+      </section>
+
+      {/* <section className="testimonial-section">
+        <div className="testimonial-header">
+          <h2>What our customers are saying</h2>
+          <div className="testimonial-nav">
+            <button onClick={handlePrev} className="nav-btn">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="size-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18"
+                />
+              </svg>
+            </button>
+            <button onClick={handleNext} className="nav-btn">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3"
+                ></path>
+              </svg>
+            </button>
+          </div>
+        </div>
+
+        <div className="testimonial-wrapper">
+          {visibleTestimonials.map((testimonial, idx) => (
+            <div className="testimonial-card" key={idx}>
+              <img
+                src={testimonial.image}
+                alt={testimonial.name}
+                className="testimonial-image"
+              />
+              <div className="testimonial-content">
+                <p className="testimonial-feedback">"{testimonial.feedback}"</p>
+                <div className="testimonial-meta">
+                  <div>
+                    <strong className="testimonial-name">
+                      {testimonial.name}
+                    </strong>
+                    <div className="testimonial-role">{testimonial.role}</div>
+                  </div>
+                  <img
+                    src={testimonial.companyLogo}
+                    alt="Company logo"
+                    className="company-logo"
+                  />
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </section> */}
+      <section className="testimonial-section">
+        <div className="testimonial-header">
+          <h2>What our customers are saying</h2>
+          <div className="testimonial-nav">
+            <button onClick={handlePrev} className="nav-btn">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="size-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18"
+                ></path>
+              </svg>
+            </button>
+            <button onClick={handleNext} className="nav-btn">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="size-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3"
+                ></path>
+              </svg>
+            </button>
+          </div>
+        </div>
+
+        <div className="testimonial-wrapper">
+          {visibleTestimonials.map((t, idx) => (
+            <div className="testimonial-card" key={idx}>
+              <img src={t.image} alt={t.name} className="testimonial-image" />
+              <div className="testimonial-content">
+                <p className="testimonial-feedback">"{t.feedback}"</p>
+                <div className="testimonial-meta">
+                  <div>
+                    <strong className="testimonial-name">{t.name}</strong>
+                    <div className="testimonial-role">{t.role}</div>
+                  </div>
+                  <img
+                    src={t.companyLogo}
+                    alt="Company logo"
+                    className="company-logo"
+                  />
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
+
+      <Footer />
     </main>
   );
 }
---
+++ src/components/footer/page.js
@@ -0,0 +1,85 @@
+"use client";
+import Image from "next/image";
+import "./footer.css";
+import {
+  FaInstagram,
+  FaFacebookF,
+  FaLinkedinIn,
+  FaTimes,
+} from "react-icons/fa";
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer-top">
+        <h2 className="footer-heading">It’s time to elevate your business</h2>
+        <div className="get-in-touch">Get in touch</div>
+      </div>
+
+      <div className="footer-main">
+        <div className="footer-left">
+          <Image
+            src="./check.png"
+            width={0}
+            height={0}
+            unoptimized
+            alt="footer-img"
+          ></Image>
+          <div className="newsletter-formm">
+            <p>
+              Address : SF-40 2nd Floor, Pearl Omaxe, Netaji Subhash Place,
+              Pitampura, New Delhi - 110034
+            </p>
+          </div>
+
+          <div className="footer-social">
+            <a href="#">
+              <FaInstagram />
+            </a>
+            <a href="#">
+              <FaFacebookF />
+            </a>
+            <a href="#">
+              <FaLinkedinIn />
+            </a>
+            <a href="#">
+              <FaTimes />
+            </a>
+          </div>
+        </div>
+
+        <div className="footer-links">
+          <div className="link-column">
+            <h4>Page link</h4>
+            <a href="#" className="active">
+              Home
+            </a>
+            <a href="#">Home 2</a>
+            <a href="#">About</a>
+            <a href="#">Contact</a>
+            <a href="#">Blog</a>
+          </div>
+          <div className="link-column">
+            <h4>Page link</h4>
+
+            <a href="#">Feature</a>
+            <a href="#">Careers</a>
+            <a href="#">Pricing</a>
+            <a href="#">Integrations</a>
+          </div>
+          <div className="link-column">
+            <h4>Utility Pages</h4>
+            <a href="#">Style Guide</a>
+            <a href="#">Instructions</a>
+            <a href="#">Licenses</a>
+            <a href="#">Changelog</a>
+            <a href="#">Link in Bio</a>
+            <a href="#">Coming Soon</a>
+            <a href="#">Error 404</a>
+            <a href="#">Password Protected</a>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
