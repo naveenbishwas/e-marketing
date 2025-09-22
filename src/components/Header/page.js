2772c5f1d41fbaf7b7b8ebfd88f9899264819ae5
naveenbishwas <naveenbishwas4@gmail.com>
2025-09-22 17:00:06 +0530

    text

--- src/components/Header/page.js
+++ src/components/Header/page.js
@@ -31,10 +31,24 @@ const Header = () => {
 
         <nav className={`nav-menu ${menuOpen ? "open" : ""}`}>
           <ul>
-            <Link href="/">
-              <li className="active">
-                <span>•</span> Home
-              </li>
+            <Link href="tel:+919315003754">
+              <div className="phone">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="phone-icon"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="1.8"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z"
+                  />
+                </svg>
+                <span className="phone-text">+91 931 500 3754</span>
+              </div>
             </Link>
             <Link href="case-studies">
               <li>Case Studies</li>
@@ -46,7 +60,7 @@ const Header = () => {
         </nav>
 
         <div className="header-right">
-          <a href="tel:+919315003754">
+          <Link href="tel:+919315003754">
             <div className="phone">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -64,9 +78,9 @@ const Header = () => {
               </svg>
               <span className="phone-text">+91 931 500 3754</span>
             </div>
-          </a>
+          </Link>
 
-          <a href="https://calendly.com/sayam-unnity/30min">
+          <Link href="https://calendly.com/sayam-unnity/30min">
             <button className="cta-button">
               Start now
               <span className="arrow-wrapper">
@@ -102,7 +116,7 @@ const Header = () => {
                 </span>
               </span>
             </button>
-          </a>
+          </Link>
         </div>
       </header>
     </>
